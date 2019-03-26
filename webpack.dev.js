7478f029302c54888add21d185c7912b21665af4
Geoffrey Benson <xaroth@gmail.com>
2019-03-25 18:22:59 -0700

    Builds with webpack now, in preparation for using wasm-loader

---
+++ webpack.dev.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const baseDir = path.resolve(__dirname);
+const srcDir = path.resolve(baseDir, 'lib');
+const buildDir = path.resolve(baseDir, 'build');
+const modulesDir = path.join(baseDir, 'node_modules');
+
+module.exports = {
+    mode: 'development',
+
+    entry: [
+        'views/Planet.js'
+    ],
+
+    output: {
+        path: buildDir,
+        filename: 'index.js',
+        publicPath: '',
+        library: 'ReactPlanet',
+        libraryTarget: 'umd'
+    },
+
+    externals: {
+        react: 'react'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-react'
+                    ]
+                },
+                include: srcDir,
+                exclude: modulesDir
+            },
+            {
+                test: /\.wasm$/,
+                loaders: ['wasm-loader']
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js'],
+        modules: [
+            srcDir,
+            'node_modules'
+        ]
+    },
+
+    watch: true
+};
