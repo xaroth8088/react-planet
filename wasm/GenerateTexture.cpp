e2499b8f14ec273334d6dcedf943c4c04f354a36
Geoffrey Benson <xaroth@gmail.com>
2019-04-13 19:59:17 -0700

    Pass in the seed

--- wasm/GenerateTexture.cpp
+++ wasm/GenerateTexture.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
 #include <emscripten.h>
 #include "OpenSimplexNoise.h"
 
 using namespace OpenSimplexNoise;
 
 extern "C" {
-    double EMSCRIPTEN_KEEPALIVE getNoise(double x, double y, double z) {
-        Noise noise;
+    double EMSCRIPTEN_KEEPALIVE getNoise(double seed, double x, double y, double z) {
+        Noise noise(seed);
+//        Noise noise;
         return noise.eval(x, y, z);
     }
 }
