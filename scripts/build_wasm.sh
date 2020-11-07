06d21a12e7cf29cfa7822cb55ca62ea2d6470ef0
Geoffrey Benson <xaroth@gmail.com>
2020-11-07 15:10:28 -0800

    First attempt at SIMD usage

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -16,14 +16,21 @@ emcc \
     -s SINGLE_FILE=1 \
     -s USE_ES6_IMPORT_META=0 \
     -s EXPORT_ES6=1 \
+    -s MODULARIZE=1 \
     --no-entry \
-    -Os \
+    -msimd128 \
+    -O3 \
     -fno-exceptions \
     --bind \
-    -o wasm_build/GenerateTexture.mjs \
+    -o wasm_build/GenerateTexture.js \
     emscripten/GenerateTexture.cpp \
     emscripten/EmscriptenWrapper.cpp \
     wasm/OpenSimplexNoise.cpp \
     wasm/TextureGenerator.cpp \
     wasm/NoiseWrapper.cpp
-cp wasm_build/GenerateTexture.mjs lib/GenerateTexture.js
+cp wasm_build/GenerateTexture.js lib/GenerateTexture.js
+cp wasm_build/GenerateTexture.wasm.map demo-page/public/GenerateTexture.wasm.map
+
+# For adding source maps
+#    -g4 \
+#    --source-map-base http://localhost:3000/ \
