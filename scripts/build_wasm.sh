cd64c60eae09ca74edc3f746a42efe83ba2f20dd
Geoffrey Benson <xaroth@gmail.com>
2020-11-08 19:10:51 -0800

    Updates to build script for compatibility with emscripten 2.0.8

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -2,13 +2,12 @@
 cd "$(dirname "$0")/.."
 mkdir -p wasm_build
 source ../emsdk/emsdk_env.sh
-emcc \
+em++ \
     -std=c++17 \
     -s WASM=1 \
-    -s ENVIRONMENT="'web'" \
+    -s ENVIRONMENT=web \
     -s ASSERTIONS=0 \
-    -s INVOKE_RUN=0 \
-    -s MALLOC="'emmalloc'" \
+    -s MALLOC=emmalloc \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s FILESYSTEM=0 \
     -s EMIT_EMSCRIPTEN_METADATA=1 \
@@ -17,8 +16,8 @@ emcc \
     -s USE_ES6_IMPORT_META=0 \
     -s EXPORT_ES6=1 \
     -s MODULARIZE=1 \
+    -s INVOKE_RUN=0 \
     --no-entry \
-    -msimd128 \
     -O3 \
     -fno-exceptions \
     --bind \
@@ -29,8 +28,17 @@ emcc \
     wasm/TextureGenerator.cpp \
     wasm/NoiseWrapper.cpp
 cp wasm_build/GenerateTexture.js lib/GenerateTexture.js
-cp wasm_build/GenerateTexture.wasm.map demo-page/public/GenerateTexture.wasm.map
 
-# For adding source maps
+# To enable SIMD autovectorization...
+#    -msimd128 \
+
+# To enable debugging via the demo page...
+#    -s SINGLE_FILE=0 \
 #    -g4 \
-#    --source-map-base http://localhost:3000/ \
+#    --source-map-base /react-planet/static/js/ \
+
+# mkdir -p demo-page/public/static/js
+# cp -R emscripten demo-page/public/static/js/
+# cp -R wasm demo-page/public/static/js/
+# cp wasm_build/GenerateTexture.wasm demo-page/public/static/js/GenerateTexture.wasm
+# cp wasm_build/GenerateTexture.wasm.map demo-page/public/static/js/GenerateTexture.wasm.map
